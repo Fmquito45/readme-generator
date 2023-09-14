1f06dfcd6f172f3e092167aaa57c238e9c1cc50c
Fmquito45 <fmquitox45@gmail.com>
2023-09-14 09:42:19 -0400

    adds couple questions to test prompt

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./generateMarkdown');
+//const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter your project titile',
+        name: 'userTitle',
+    },
+    {
+        type: 'input',
+        message: 'Provide a short discription explaning the what, why, and how of your project. Use the following questions as a guide: \n What was your motivation? \n Why did you build this project? \n What problem does it solve? \n What did you learn?',
+        name: 'description',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
