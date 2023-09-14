89e220860eb8cd26a60f42b97587aa2c570515d4
Fmquito45 <fmquitox45@gmail.com>
2023-09-14 17:57:22 -0400

    adds questions to question variable

--- index.js
+++ index.js
@@ -5,14 +5,50 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type: 'input',
-        message: 'Enter your project titile',
         name: 'userTitle',
+        message: 'Enter your project titile.\n',
     },
     {
         type: 'input',
-        message: 'Provide a short discription explaning the what, why, and how of your project. Use the following questions as a guide: \n What was your motivation? \n Why did you build this project? \n What problem does it solve? \n What did you learn?',
         name: 'description',
-    }
+        message: 'Provide a short discription explaning the what, why, and how of your project. Use the following questions as a guide: \n What was your motivation? \n Why did you build this project? \n What problem does it solve? \n What did you learn?\n',
+    },
+    {
+        type: 'input',
+        name: 'istallation',
+        message: 'What are the steps required to install your project? Provide a command to run or a step-by-step description of how to get the development environment running.\n',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.\n', 
+    },
+    {
+        type: 'list',
+        name: 'license',
+        choices: ['Apache License 2.0', 'MIT License', 'Mozilla Public License 2.0'],
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Include guidelines for others to contribute.\n', 
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Go the extra mile and write tests for your application. Provide commands on how to run them here.\n',
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'Enter your GitHub Username.\n',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your Email address.\n',
+    },
+
 ];
 
 // TODO: Create a function to write README file
